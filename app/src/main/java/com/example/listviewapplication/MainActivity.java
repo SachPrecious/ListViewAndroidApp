e7376bdc080756127a1cfd8e929ef788c03f00c8
SachPrecious <sachithrabandara49@gmail.com>
2022-04-07 11:55:38 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/listviewapplication/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.listviewapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String[] countries =new String[]{""};
+    }
+}
