9500db312178125340d9d955589d524831f56cfd
SachPrecious <sachithrabandara49@gmail.com>
2022-04-07 12:33:33 +0530

    Commit 3

--- app/src/main/java/com/example/listviewapplication/MainActivity.java
+++ app/src/main/java/com/example/listviewapplication/MainActivity.java
@@ -3,14 +3,24 @@ package com.example.listviewapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String[] countries =new String[]{""};
+        String[] countries =new String[]{"Sri Lanka","New Zealand","England","Australia","USA","Sri Lanka","New Zealand","England","Australia","USA"};
+
+        ListAdapter listAdapter = new ArrayAdapter<String>(this,R.layout.list_item,countries);
+        listView=findViewById(R.id.list_view);
+        listView.setAdapter(listAdapter);
+
     }
 }
